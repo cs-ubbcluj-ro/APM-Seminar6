02215b9756c4dab7ddea50e67e95d71acd1d8988
ibocicor <ibocicor@gmail.com>
2023-12-19 10:21:39 +0200

    initial version

---
+++ src/main/Main.java
@@ -0,0 +1,23 @@
+package main;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main extends Application {
+    public static void main(String[] args) {
+//        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+//        FXMLLoader loader = new FXMLLoader(getClass().getResource("/threads/gui.fxml"));
+//        stage.setTitle("Threads");
+//        stage.setScene(new Scene(loader.load(), 750, 400));
+//        stage.sizeToScene();
+//        stage.show();
+    }
+}
